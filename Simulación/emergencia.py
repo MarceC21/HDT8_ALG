5b55288b2c44c18d2dd20faba868d88c7f845f2c
JorgeVilledaS <vil24932@uvg.edu.gt>
2025-04-01 18:06:29 -0600

    Clase emergencia

--- Simulación/emergencia.py
+++ Simulación/emergencia.py
@@ -12,28 +12,10 @@ Descripción: Clase modelo de cada emergencia.
 """
 import random
 
-# EMERGENCIA: numero, recursos (servicios de lab, equipos, etc.), doctor disponible
-
-
-
-def generar_paciente(env, nombre, enfermedad, cola_procesos):
-    """ Simula el ciclo de vida de un proceso en un sistema operativo """
-    memoria_requerida = random.randint(1, 10)
-    instrucciones_totales = random.randint(1, 10)
-
-    print(f"{env.now}: {nombre} llega al sistema (requiere {memoria_requerida} de RAM)")
-
-    # Solicitar RAM (espera hasta obtenerla)
-    yield ram.get(memoria_requerida)
-    print(f"{env.now}: {nombre} obtiene {memoria_requerida} de RAM y está listo para ejecutarse")
-
-    # Agregar proceso a la cola para el CPU (espera en la cola hasta que CPU lo atienda)
-    cola_procesos.append((nombre, instrucciones_totales, memoria_requerida))
-
-def iniciar_simulacion(env, ram, num_procesos, intervalo_llegada, cola_procesos):
-    """ Genera procesos siguiendo una distribución exponencial """
-    for i in range(num_procesos):
-        env.process(generar_proceso(env, f"Proceso-{i+1}", ram, cola_procesos))
-        yield env.timeout(random.expovariate(1.0 / intervalo_llegada))
-
-
+class Emergencia:
+    def __init__(self, id_paciente):
+        self.id_paciente = id_paciente
+        self.severidad = random.randint(1, 5)  # Nivel de urgencia (1 es más grave)
+    
+    def __str__(self):
+        return f"Paciente {self.id_paciente} - Severidad {self.severidad}"
